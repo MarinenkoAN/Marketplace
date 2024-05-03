6712becfbd6d9a79e4fe9a42f0732ca1731f6385
Diablo <Diablo@DESKTOP-DUGL2NS>
2024-05-03 13:06:42 +0700

    Добавьте файлы проекта.

---
+++ CourseProject(WPF)/ViewModel/VK/Vk.cs
@@ -0,0 +1,65 @@
+﻿using CourseProject_WPF_.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+
+namespace CourseProject_WPF_
+{
+    public class Vk
+    {
+        VkApi vkApi = new VkApi();
+        const ulong AppId = 4268118;
+
+        public Vk(string login, string password, string code)
+        {
+            auth(login, password, code);
+        }
+
+        public void auth(string login, string password, string code)
+        {
+            vkApi.Authorize(new ApiAuthParams
+            {
+                ApplicationId = AppId,
+                Login = login,
+                Password = password,
+                Settings = Settings.All,
+                TwoFactorAuthorization = () =>
+                {
+                    //Enter enter = new Enter();
+                    //enter.ShowDialog();
+                    //return enter.getCode();
+                    return code;
+                }
+
+            });
+        }
+
+        public User getUser()
+        {
+            var p = vkApi.Users.Get(
+                new long[] { vkApi.UserId.Value },
+                ProfileFields.Contacts | ProfileFields.Country |
+                ProfileFields.City | ProfileFields.About |
+                ProfileFields.Photo200Orig).FirstOrDefault();   
+            
+            return p;
+        }
+
+        public BitmapImage getImage()
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = getUser().Photo200Orig;
+            bitmapImage.EndInit();
+            return bitmapImage;
+        }
+    }
+}
---
+++ CourseProject(WPF)/ViewModel/VK/VkLoginViewModel.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.ComponentModel;
+using CourseProject_WPF_.Model;
+using CourseProject_WPF_.View;
+using CourseProject_WPF_.Repositories;
+using System.Net;
+using System.Windows;
+
+namespace CourseProject_WPF_.ViewModel
+{
+    public class VkLoginViewModel : INotifyPropertyChanged
+    {        
+        EFUserRepository userRepository = new EFUserRepository();
+
+        string login;                
+        string status;
+        string code;
+
+        public string Login
+        {
+            get { return login; }
+            set
+            {
+                login = value;
+                OnPropertyChanged("Login");
+            }
+        }
+        public string Code
+        {
+            get { return code; }
+            set
+            {
+                code = value;
+                OnPropertyChanged("Code");
+            }
+        }
+
+        public string Status
+        {
+            get { return status; }
+            set
+            {
+                status = value;
+                OnPropertyChanged("Status");
+            }
+        }
+
+        public bool auth(string password)
+        {
+            try
+            {
+                Status = "Подождите нексолько секунд";
+                Vk vk = new Vk(Login, password, Code);
+                VkNet.Model.User user = vk.getUser();                
+
+                string firstName = "";
+                string lastName = "";
+                string telNumber = "";
+                string about = "";
+                byte[] image;
+                string i="";
+
+                if (userRepository.getByMail(Login) != null)
+                {
+                    firstName = user.FirstName;
+                    lastName = user.LastName;
+                    telNumber = user.MobilePhone; ;
+                    about = $"Страна: {user.Country.Title}\n" + $"Город: {user.City.Title}";
+
+                    using (WebClient client = new WebClient())
+                    {
+                        client.DownloadFile(new Uri(user.Photo200Orig.AbsoluteUri), User.filename);
+                    }
+                    using (System.IO.FileStream fs = new System.IO.FileStream(User.filename, System.IO.FileMode.OpenOrCreate))
+                    {
+                        image = new byte[fs.Length];
+                        fs.Read(image, 0, image.Length);
+                    }
+                    CurrentUser.User = userRepository.getByMail(Login);
+                    User tmp = new User(firstName, lastName, Login, telNumber, telNumber, about, image, CurrentUser.User.Privilege);
+                    userRepository.update(CurrentUser.User, tmp);
+
+                    App.authWindow.Close();
+                    App.mainWindow = new MainWindow();
+                    App.mainWindow.Show();
+                    image = null;
+                    return true;
+                }
+                else
+                {
+                    firstName = user.FirstName;
+                    lastName = user.LastName;
+                    telNumber = "";                    
+                    about = $"Страна: {user.Country.Title}\n"+ $"Город: {user.City.Title}";
+                    using (WebClient client = new WebClient())
+                    {
+                        client.DownloadFile(new Uri(user.Photo200Orig.AbsoluteUri), User.filename);
+                    }
+                    using (System.IO.FileStream fs = new System.IO.FileStream(User.filename, System.IO.FileMode.Open))
+                    {
+                        image = new byte[fs.Length];
+                        fs.Read(image, 0, image.Length);
+                    }
+
+                    userRepository.add(new Model.User(firstName, lastName, Login, telNumber,about,image));
+                    CurrentUser.User = userRepository.getByMail(Login);
+
+                    App.authWindow.Close();
+                    App.mainWindow = new MainWindow();
+                    App.mainWindow.Show();
+                    image = null;
+                    return true;
+                    
+                }
+
+            }
+            catch (VkNet.Exception.UserAuthorizationFailException)
+            {
+                Status = "Введите код подверждения и повторите вход";
+            }
+            catch (VkNet.Exception.VkApiException)
+            {
+                Status = "Неверно указан логин или пароль";
+            }
+            catch (Exception)
+            {
+                Status = "Ошибка";
+            }
+            return false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
